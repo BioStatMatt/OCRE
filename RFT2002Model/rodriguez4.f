891cc4c7823e1e3177992baba09e83f7d1ffb206
Matthew S. Shotwell <biostatmatt@gmail.com>
2014-05-10 07:20:12 -0500

    updates from Sunil; plateau from 15 to 12 min; eliminate alternans

--- RFT2002Model/rodriguez4.f
+++ RFT2002Model/rodriguez4.f
@@ -1063,9 +1063,7 @@ c 	Formulation of Ikatp proposed by Ferrero et al.
 
 	sigmakatp = 3.8
 	rhokatp   = 0.91
-	fatpfinal = 0.008
-	
-	gkatp(i,j) = gamakatp(i,j)*fmatp(i,j)*fnatp(i,j)*ft
+	fatpfinal = 0.004
 	
 	gamakatp(i,j) = 35.375*(ke(i,j)/5.4)**0.24
 
@@ -1075,12 +1073,10 @@ c 	Formulation of Ikatp proposed by Ferrero et al.
 	fnatp(i,j) = 1/(1 + (Nai(i,j)/khna(i,j))**2)
 	khna(i,j) = 25.9*exp((-0.35/RTF)*vm(i,j))
 
-	ft = 1.3**(Temp-309)/10
+	ft = 1.3**((Temp-309)/10)
+	gkatp(i,j) = gamakatp(i,j)*fmatp(i,j)*fnatp(i,j)*ft
 
-	
-	kmatp(i,j) = (35.8 + 17.9*adp(i,j)**0.256)
-	hatp(i,j) = 1.3 + 0.74*exp(-0.09*adp(i,j))
-	fatp(i,j) =fatpfactor/(1 + (atp(i,j)/kmatp(i,j))**hatp(i,j))
+        fatp(i,j) = fatpfactor	
 
 	ekatp(i,j) = RTF*log(ke(i,j)/ki(i,j))
 
@@ -1233,7 +1229,7 @@ c           write(6,*) ' dvdtmax now ',k*dt,i,j
 
            dcaitwo(i,j) = caitwo(i,j) - caizero(i,j)
 	
-	    iclock(i,j) = 0
+	    iclock(i,j) = 2 
 	
            If (dcaitwo(i,j).gt.dcaith) then
               grelcicrbar(i,j)= 60.
